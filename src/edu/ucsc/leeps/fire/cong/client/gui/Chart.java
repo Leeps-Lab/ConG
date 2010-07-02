6dc5b055db45b99cb2938b1426e3414286de9417
rlou <rlou>
2010-07-02 23:15:53 +0000

    Shifted shock zones
    git-svn-id: file:///svn/CONGv2.0/trunk@1495 637b740e-b1db-41bf-a134-2c17f54ac410

--- src/edu/ucsc/leeps/fire/cong/client/gui/Chart.java
+++ src/edu/ucsc/leeps/fire/cong/client/gui/Chart.java
@@ -155,7 +155,7 @@ public class Chart extends Sprite implements Configurable<Config> {
             applet.fill(100, 100, 100, 50);
             applet.noStroke();
             float x0, y0, x1, y1;
-            x0 = width * FIRE.client.getConfig().shock.start;
+            x0 = width * FIRE.client.getConfig().shock.start - 1;
             y0 = 0;
             x1 = width * FIRE.client.getConfig().shock.end;
             y1 = scaledHeight + scaledMargin * 2;
