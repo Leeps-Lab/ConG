5fbefff84ab7b89793180ac9c9f2b53fac5c705b
jpettit <etherealmachine@gmail.com>
2010-07-21 18:01:16 +0000

    git-svn-id: file:///svn/CONGv2.0/trunk@1555 637b740e-b1db-41bf-a134-2c17f54ac410

--- src/edu/ucsc/leeps/fire/cong/Main.java
+++ src/edu/ucsc/leeps/fire/cong/Main.java
@@ -1,9 +1,6 @@
 package edu.ucsc.leeps.fire.cong;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import edu.ucsc.leeps.fire.testing.Startup;
 
 /**
  *
@@ -11,64 +8,7 @@ import java.io.InputStreamReader;
  */
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        startClient();
-        startClient();
-        startClient();
-        startClient();
-        startServer().waitFor();
-    }
-
-    private static Process startServer() throws IOException {
-        Runtime runtime = Runtime.getRuntime();
-        Process p = runtime.exec(new String[]{"java", "-ea", "-jar", "dist/Server.jar"});
-        runtime.addShutdownHook(new ShutdownThread(p));
-        new OutputRedirectThread(p.getErrorStream()).start();
-        return p;
-    }
-
-    private static Process startClient() throws IOException {
-        Runtime runtime = Runtime.getRuntime();
-        Process p = runtime.exec(new String[]{"java", "-ea", "-jar", "dist/Client.jar"});
-        runtime.addShutdownHook(new ShutdownThread(p));
-        new OutputRedirectThread(p.getErrorStream()).start();
-        return p;
-    }
-
-    private static class OutputRedirectThread extends Thread {
-
-        private InputStream is;
-
-        public OutputRedirectThread(InputStream is) {
-            this.is = is;
-        }
-
-        @Override
-        public void run() {
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                String line = reader.readLine();
-                while (line != null) {
-                    System.err.println(line);
-                    line = reader.readLine();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-
-    private static class ShutdownThread extends Thread {
-
-        private Process process;
-
-        public ShutdownThread(Process process) {
-            this.process = process;
-        }
-
-        @Override
-        public void run() {
-            process.destroy();
-        }
+    public static void main(String[] args) {
+        Startup.main(args, 2);
     }
 }
