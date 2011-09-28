a22bd75cf460df7efc8c98e3b61c61ff63b00408
developer <developer>
2011-09-28 21:10:14 +0000

    git-svn-id: file:///svn/CONGv2.0/branches/InfoDelayChart@2151 637b740e-b1db-41bf-a134-2c17f54ac410

--- configs/hotelling.js
+++ configs/hotelling.js
@@ -1,3 +1,5 @@
+importClass(java.lang.System)
+
 function getMin() {
     return 0;
 }
@@ -15,12 +17,37 @@ function getSubperiodBonus(subperiod, config) {
 }
 
 function getPayoff(id, percent, popStrategies, matchPopStrategies, config) {
-    var total = 0;
-    for (key in popStrategies.keySet()) {
-        total += popStrategies.get(key)[0];
+    var sorted = []
+    var yourStrategy
+    for (key in popStrategies.keySet().toArray()) {
+        var s = popStrategies.get(popStrategies.keySet().toArray()[key])[0]
+        if (popStrategies.keySet().toArray()[key] == id) {
+            yourStrategy = s
+        }
+        sorted.push(s)
     }
-    var mean = total / popStrategies.size();
-    return 100 * mean;
+    sorted.sort(function(a, b) {return a - b})
+
+    var i
+    for (j in sorted) {
+        if (sorted[j] == yourStrategy) {
+            i = j
+        }
+    }
+    i = parseInt(i)
+    
+  var left, right
+  if (i == 0) {
+    left = 0
+    right = sorted[i] + 0.5 * (sorted[i + 1] - sorted[i])
+  } else if (i == sorted.length - 1) {
+    left = sorted[i] - 0.5 * (sorted[i] - sorted[i - 1])
+    right = 1
+  } else {
+    left = sorted[i] - 0.5 * (sorted[i] - sorted[i - 1])
+    right = sorted[i] + 0.5 * (sorted[i + 1] - sorted[i])
+  }
+  return 100 * (sorted[i] - left) + (right - sorted[i])
 }
 
 function getPopStrategySummary(id, percent, popStrategies, matchPopStrategies) {
