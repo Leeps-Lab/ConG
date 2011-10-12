feb661e251045de5f32fa67f5b755eda37afdfd6
jpettit <etherealmachine@gmail.com>
2011-10-12 19:25:30 +0000

    Fixed showing subperiod payoffs in bubbles.
    git-svn-id: file:///svn/CONGv2.0/branches/InfoDelayChart@2166 637b740e-b1db-41bf-a134-2c17f54ac410

--- src/edu/ucsc/leeps/fire/cong/client/gui/BubblesSelector.java
+++ src/edu/ucsc/leeps/fire/cong/client/gui/BubblesSelector.java
@@ -130,11 +130,16 @@ public class BubblesSelector extends Sprite implements Configurable<Config>, Sel
         min = config.payoffFunction.getMin();
         max = config.payoffFunction.getMax();
         float payoff;
-        float[] strategy = Client.state.strategies.get(id);
+        float[] strategy;
         if (config.subperiods != 0) {
+            if (Client.state.strategiesTime.isEmpty()) {
+                return;
+            }
             payoff = config.payoffFunction.getPayoff(
                     id, 0, Client.state.getFictitiousStrategies(FIRE.client.getID(), subperiodStrategy), null, config);
+            strategy = Client.state.strategiesTime.get(Client.state.strategiesTime.size() - 1).strategies.get(id);
         } else {
+            strategy = Client.state.strategies.get(id);
             payoff = PayoffUtils.getPayoff(id, strategy);
         }
         x = width * strategy[0];
